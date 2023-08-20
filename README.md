ce1ca85d61469b10417152000d6d387def40c641
gabigab117 <gabrieltrouve5@outlook.com>
2023-08-20 20:07:20 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # compress
- 
+ - uploader une image
+ - compresser avec pillow
+ - changer la taille, enlever les meta, réduire la qualité
+
+# procéder
+- formulaire dans lequel j'up une image
+- elle va être traiter selon certain paramètres prédef
--- project/compressor/models.py
+++ project/compressor/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
+
+from project.settings import AUTH_USER_MODEL
 
 
+class Image(models.Model):
+    image = models.ImageField(upload_to="upImage")
+    slug = models.SlugField()
+    user = models.ForeignKey(to=AUTH_USER_MODEL, on_delete=models.CASCADE,
+                             verbose_name="Utilisateur", null=True)
+
+    def __str__(self):
+        return f"{self.user}{self.slug}"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.image.name)
+        super().save(*args, **kwargs)
