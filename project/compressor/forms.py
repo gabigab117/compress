7a357653e60a5d727a6ba28defd88f8b0b560991
gabigab117 <gabrieltrouve5@outlook.com>
2023-08-20 20:10:58 +0200

    init image form

--- project/compressor/forms.py
+++ project/compressor/forms.py
@@ -3,6 +3,9 @@ from .models import Image
 
 
 class UploadImage(forms.ModelForm):
+    width = forms.IntegerField(label="Largeur")
+    height = forms.IntegerField(label="Hauteur")
+
     class Meta:
         model = Image
         fields = ["image"]
