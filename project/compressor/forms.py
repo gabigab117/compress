84236400307ef5228e26dfa634b4c2f8cd4cfdc5
gabigab117 <gabrieltrouve5@outlook.com>
2023-08-29 21:27:02 +0200

    premium form and view update

--- project/compressor/forms.py
+++ project/compressor/forms.py
@@ -20,9 +20,10 @@ class Compress(forms.Form):
 
 
 class PremiumForm(forms.ModelForm):
-    quality = forms.IntegerField(label="Qualité", min_value=1, max_value=100)
+    quality = forms.IntegerField(label="Qualité", min_value=1, max_value=95)
     width = forms.IntegerField(label="Largeur", required=False)
     height = forms.IntegerField(label="Hauteur", required=False)
 
     class Meta:
+        model = UpImage
         fields = ["quality", "width", "height"]
