ac5b3b495a165dc6ee895a1b06bc3f82966a8d67
gabriel <gabrieltrouve5@gmail.com>
2023-12-01 13:17:26 +0100

    add required

--- project/compressor/forms.py
+++ project/compressor/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.core.exceptions import ValidationError
 
 from .models import UpImage
 
@@ -20,7 +19,7 @@ class Compress(forms.Form):
 
 
 class PremiumForm(forms.ModelForm):
-    quality = forms.IntegerField(label="Qualité", min_value=1, max_value=95)
+    quality = forms.IntegerField(label="Qualité", min_value=1, max_value=95, required=True)
     width = forms.IntegerField(label="Largeur", required=False)
     height = forms.IntegerField(label="Hauteur", required=False)
 
@@ -39,6 +38,5 @@ class PremiumDeleteForm(forms.ModelForm):
     def save(self, *args, **kwargs):
         if self.cleaned_data["delete"]:
             self.instance.delete()
-            # Stopper la méthode pour ne pas sauvegarder avec un return
             return True
         return super().save(*args, **kwargs)
